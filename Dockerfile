4cdc747e78c8b1e168c282bb26b78e773410c400
yunii2222 <yunii2222@naver.com>
2024-05-20 12:25:59 +0900

    변경

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM ubuntu:latest
-LABEL authors="hi"
-
-ENTRYPOINT ["top", "-b"]
+# 버전
+FROM openjdk:17
+# 경로
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+# 구동시킨다.
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/org/example/demo/TestController.java
+++ src/main/java/org/example/demo/TestController.java
@@ -1,2 +1,13 @@
-package org.example.demo;public class TestController {
+package org.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("Hello Docker!!!");
+    }
 }
